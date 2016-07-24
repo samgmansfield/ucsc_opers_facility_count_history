881dc58b6aa04df25115341fbbd37475c29c7b1f
Sam Mansfield <smansfie@slamdance.soe.ucsc.edu>
2016-07-23 21:33:06 -0700

    Changed datetime to a string before trying to write it.

--- get_facility_counts.py
+++ get_facility_counts.py
@@ -7,7 +7,8 @@ while(True):
     r = requests.get('https://docs.google.com/spreadsheets/u/1/d/1o1lQ6FFqr6RALPZ6I48cuWDd1clrPOlks8f3sWKx-9s/pubhtml?gid=217361177')
     
     f = open('facility_counts.txt', 'a')
-    f.write(datetime.now() + "\n") 
+    f.write(str(datetime.now()))
+    f.write("\n")
    
     # m[0]: name of room, m[1]: number of people
     for m in re.findall(r'\[\{\"v\":\"([a-zA-Z0-9 ]+)\"\},\{\"v\":(\d+)\.(\d+),\"f\":\"(\d+)\"\}\]', r.text):
