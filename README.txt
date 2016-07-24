11640206f3b510a8ff2a5acf73fa90599dba5e42
Sam Mansfield <samgmansfield@gmail.com>
2016-07-23 18:12:19 -0700

    Initial commit. I created the script that gets the information from the UCSC OPERS Live Facility Count Google Doc every 30 minutes.

---
+++ README.txt
@@ -0,0 +1,6 @@
+Author: Sam Mansfield
+
+facility_counts.txt: Stores the facility counts.
+get_facility_counts.py: Gets the UCSC OPERS live facility count from the 
+  Google Doc posted on the OPERS website and stores into the file 
+  facility_counts.txt
---
+++ get_facility_counts.py
@@ -0,0 +1,18 @@
+import requests
+from datetime import datetime
+import re
+import time
+
+while(True):
+    r = requests.get('https://docs.google.com/spreadsheets/u/1/d/1o1lQ6FFqr6RALPZ6I48cuWDd1clrPOlks8f3sWKx-9s/pubhtml?gid=217361177')
+    
+    f = open('facility_counts.txt', 'a')
+    f.write(datetime.now() + "\n") 
+   
+    # m[0]: name of room, m[1]: number of people
+    for m in re.findall(r'\[\{\"v\":\"([a-zA-Z0-9 ]+)\"\},\{\"v\":(\d+)\.(\d+),\"f\":\"(\d+)\"\}\]', r.text):
+        f.write(m[0] + "\n")
+        f.write(m[1] + "\n")
+    
+    f.close()
+    time.sleep(30*60)
